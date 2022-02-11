431b68b01121aeef835810d60cf61710e9f9abaf
SaadMakhalSaad2 <gosaadmakhal@gmail.com>
2022-02-11 20:47:30 +0200

    f

--- js/workspace.js
+++ js/workspace.js
@@ -1,15 +1,49 @@
-import { getAuth, createUserWithEmailAndPassword, signOut } from "https://www.gstatic.com/firebasejs/9.6.5/firebase-auth.js";
+import { getAuth, signOut } from "https://www.gstatic.com/firebasejs/9.6.5/firebase-auth.js";
 import { getDatabase, ref, set, onValue } from "https://www.gstatic.com/firebasejs/9.6.5/firebase-database.js";
 import { initializeApp } from "https://www.gstatic.com/firebasejs/9.6.5/firebase-app.js";
-import { getAnalytics } from "https://www.gstatic.com/firebasejs/9.6.5/firebase-analytics.js";
 const topicTitle = document.getElementById('topic-title');
 document.addEventListener("DOMContentLoaded", function (event) {
-  console.log('workspace screen')
+  const firebaseConfig = {
+    apiKey: "AIzaSyDJ9ptySym5DgJT9wnKSbYGuZ3k9av7UsA",
+    authDomain: "pazion-d1b6c.firebaseapp.com",
+    projectId: "pazion-d1b6c",
+    storageBucket: "pazion-d1b6c.appspot.com",
+    messagingSenderId: "259375163869",
+    appId: "1:259375163869:web:b4627353698136bd292e34",
+    measurementId: "G-KKL7LHF0B5"
+  };
 
-topicTitle.innerHTML = localStorage.getItem('topic_redirect_title')
+
+  const app = initializeApp(firebaseConfig);
+  const auth = getAuth();
+  const database = getDatabase();
 
 
+  auth.onAuthStateChanged(function (user) {
+    if (user) {
+      const starCountRef = ref(database, 'questions/');
+      onValue(starCountRef, (snapshot) => {
+        const data = snapshot.val();
+        const allQuestions = json2array(data);
+        console.log(allQuestions);
+        const topicQuestions = []
+        console.log(localStorage.getItem('topic_redirect_id'))
 
+        for (let i = 0; i < allQuestions.length; i++) {
+          if (allQuestions[i].topic == localStorage.getItem('topic_redirect_id')) {
+            console.log('match')
+            topicQuestions.push(allQuestions[i])
+          }
+        }
+
+        console.log(topicQuestions)
+
+
+      });
+    } else {
+      return;
+    }
+  });
 
 
 
@@ -19,3 +53,12 @@ topicTitle.innerHTML = localStorage.getItem('topic_redirect_title')
 
 
 });
+
+function json2array(json) {
+  var result = [];
+  var keys = Object.keys(json);
+  keys.forEach(function (key) {
+    result.push(json[key]);
+  });
+  return result;
+}
